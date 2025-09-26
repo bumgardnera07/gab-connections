7f1ff32e6c175d689e4e1eaf4c8c10fe950002b2
George Bumgardner <bumgardnera07@gmail.com>
2025-09-26 10:01:16 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,26 +14,6 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="mx-auto flex max-w-screen-md flex-col gap-4 bg-stone-50 p-4 text-stone-900 sm:p-8">
-        <!-- Commenting out game header 
-        <header className="flex items-center gap-4">
-          <Link href="/" className="shrink-0">
-            <img
-              src="/logo-1024.png"
-              alt="Custom Connections Logo"
-              className="h-12 w-12"
-            />
-          </Link>
-
-          <div>
-            <Link href="/">
-              <h1 className="text-2xl font-bold">Konnections</h1>
-            </Link>
-            <p>Group words that share a common thread.</p>
-          </div>
-        </header>
-        
-        <hr className="border-stone-300" />
-        -->
         <Suspense>{children}</Suspense>
       </body>
     </html>
